f3cca41250acc8a91f995eb575fe50f2dbe14f11
lennertsoffers <lennert.soffers@hotmail.com>
2022-10-22 13:08:38 +0300

    Documented actions, error

--- src/auth/ProtectedComponent.tsx
+++ src/auth/ProtectedComponent.tsx
@@ -1,6 +1,7 @@
 import { Outlet, Navigate } from "react-router-dom";
 import AuthService from "../api/AuthService";
 
+/** Represents a component that can only be rendered if the user is logged in */
 const ProtectedComponent = () => {
     if (AuthService.isLoggedIn()) {
         return <Outlet />;
